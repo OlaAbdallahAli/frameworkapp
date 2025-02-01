ea18ed9e0e0b20b94783185f11075061f8bdfd56
OlaAbdallahAli <olaali2815@gmail.com>
2025-02-01 12:00:41 +0300

    change in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,30 +4,46 @@ export default function Navbar() {
   return (
     <>
       <div className="navbar navbar-expand-lg  fixed-top  ">
-        <div className="container  ">
+        <div className="container  d-flex">
           <NavLink
             to=""
             className={"navbar-brand text-white fw-bold text-uppercase"}
           >
             Start Framework
           </NavLink>
-          <ul className="navbar-nav  mb-2 mb-lg-0 d-flex justify-content-between ">
-            <li className="nav-item">
-              <NavLink to="about" className="nav-link text-uppercase">
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="portfolio" className="nav-link text-uppercase">
-                Portfolio
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="contact" className="nav-link text-uppercase">
-                Contact
-              </NavLink>
-            </li>
-          </ul>
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            className="navbar-toggler collapsed show"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            id="navbarSupportedContent"
+            className="navbar-collapse collapse show "
+          >
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex justify-content-between ">
+              <li className="nav-item me-3">
+                <NavLink to="about" className="nav-link text-uppercase">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item me-3">
+                <NavLink to="portfolio" className="nav-link text-uppercase">
+                  Portfolio
+                </NavLink>
+              </li>
+              <li className="nav-item me-3">
+                <NavLink to="contact" className="nav-link text-uppercase">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
